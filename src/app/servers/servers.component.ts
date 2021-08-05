a8f62ce3141e8e78c5c41203857d7cb43dc367db
MadRoosterCro <dario.car.kz@gmail.com>
2021-08-05 23:05:45 +0200

    directive intro

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer(): void {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created, name of the server is ' + this.serverName;
+  }
+  onUpdateServerName(event: any): void {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
